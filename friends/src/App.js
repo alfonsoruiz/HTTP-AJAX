f51642b75c039356bc723ffda735370ee5b2d32a
Alfonso Ruiz <1800ruiz@gmail.com>
2019-06-11 18:56:01 -0700

    setup call to friends api

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+
 import './App.css';
+import FriendsList from './components/FriendsList';
 
 class App extends React.Component {
-  contructor() {
-    super();
+
+  componentDidMount() {
+    console.log('Component did mount called');
+    axios.get('http://localhost:5000/friends')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  render() {
     return (
       <div className="App">
-       
+        <h1>Hello World</h1>
+        <FriendsList />
       </div>
     );
   }
